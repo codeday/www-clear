2d87a53947570980d78469a11076a2143c7fe605
Lola <lola@egherman.com>
2021-10-04 20:31:11 -0500

    use live graphql server

--- src/fetch.js
+++ src/fetch.js
@@ -1,14 +1,4 @@
-import {GraphQLClient} from "graphql-request";
-// import {apiFetch} from '@codeday/topo/utils' // TODO: UNCOMMENT ONCE clear-gql deployed
-
-// TODO: remove once clear-gql deployed
-
-export const apiFetch = function apiFetch(query, variables, headers) {
-    const client = new GraphQLClient('http://localhost:4000', {
-        headers: headers
-    });
-    return client.request(query, variables);
-};
+import {apiFetch} from '@codeday/topo/utils'
 
 export function useFetcher(session, variables) {
     return (q, v, h) => apiFetch(q, {...v, ...variables}, {
