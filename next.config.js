e7f53381c3cb78edffe431cd965dd5b6c7112555
Lola <lola@egherman.com>
2021-10-15 12:29:02 -0500

    bundle analysis

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+
 module.exports = {
+  ...withBundleAnalyzer({}),
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true
