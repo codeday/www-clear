2aa029c781e81a1b2ac363699dcccd11e3ce5b6a
Tyler Menezes <tylermenezes@codeday.org>
2021-10-21 09:49:21 -0400

    Show sold tickets on event list

--- src/components/Event.js
+++ src/components/Event.js
@@ -7,6 +7,7 @@ export default function Event({event, ...props}) {
         <InfoBox heading={event.name} id={event.id} as="a" href={`/events/${event.id}`} {...props}>
             <Text mb={0}>{event.eventGroup?.name || null}</Text>
             <Text mb={0}>{event.displayDate}</Text>
+            <Text mb={0}>{event.soldTickets} students</Text>
         </InfoBox>
     )
 }
