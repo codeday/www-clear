195e48205cf8b3ad870cf63daa4777349c7b272b
Shubham Patil <shubhampatilsd@gmail.com>
2021-11-07 08:50:02 -0800

    got reactiveness working and text working

--- src/components/opossum/Opossum.js
+++ src/components/opossum/Opossum.js
@@ -4,20 +4,14 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 function Opossum({ height, width }) {
-  const [renderer,setRenderer] = useState()
-
-  
+  // const [renderer, setRenderer] = useState();
+  // const [camera, setCamera] = useState();
 
   useEffect(() => {
-    const resizeWindow = ()=>{
-      renderer.setSize(window.innerWidth, height);
-    }
-    window.addEventListener('resize', resizeWindow);
-
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(
       75,
-      window.innerWidth / height,
+      window.innerWidth / (window.innerHeight / 2),
       0.1,
       1000
     );
@@ -30,23 +24,24 @@ function Opossum({ height, width }) {
     renderer.setClearColor(0x000000, 0);
 
     renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, height);
+    renderer.setSize(window.innerWidth, window.innerHeight / 2);
+
     camera.position.set(10, 2, 0);
     renderer.render(scene, camera);
 
-    setRenderer(renderer)
+    // setRenderer(renderer);
 
     const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
     hemiLight.position.set(0, 20, 0);
-    hemiLight.castShadow = true;
     scene.add(hemiLight);
 
     const dirLight = new THREE.DirectionalLight(0xffffff);
     dirLight.position.set(-3, 10, -10);
-    dirLight.castShadow = true;
+
     scene.add(dirLight);
 
     const light = new THREE.AmbientLight(0xffffff, 0.4);
+    light.castShadow = true;
     scene.add(light);
 
     const controls = new OrbitControls(camera, renderer.domElement);
@@ -56,12 +51,25 @@ function Opossum({ height, width }) {
     let opossumObj;
 
     loader.load("/Low_poly_opossum.glb", function (gltf) {
+      gltf.scene.traverse((node) => {
+        if (!node.isMesh) return;
+        node.material.wireframe = true;
+        node.material.color.set(0xff686b);
+      });
       scene.add(gltf.scene);
       opossumObj = gltf.scene;
       opossumObj.scale.set(1.5, 1.5, 1.5);
-      opossumObj.receiveShadow = true;
-      opossumObj.castShadow = true;
     });
+
+    const resizeWindow = () => {
+      renderer.setSize(window.innerWidth, window.innerHeight / 2);
+      renderer.setPixelRatio(window.devicePixelRatio);
+      camera.aspect = window.innerWidth / (window.innerHeight / 2);
+      camera.updateProjectionMatrix();
+      renderer.render(scene, camera);
+    };
+
+    window.addEventListener("resize", resizeWindow);
 
     function animate() {
       requestAnimationFrame(animate);
--- src/pages/500.js
+++ src/pages/500.js
@@ -1,6 +1,8 @@
 import Opossum from "../components/opossum/Opossum";
 import { Heading } from "@codeday/topo/Atom/Text";
+import Text from "@codeday/topo/Atom/Text";
 import Box from "@codeday/topo/Atom/Box";
+import Link from "@codeday/topo/Atom/Text/Link";
 
 export default function ServerError() {
   return (
@@ -8,7 +10,11 @@ export default function ServerError() {
       <Box style={{ display: "flex", justifyContent: "center" }}>
         <Opossum height={600} />
       </Box>
-      <Heading style={{ textAlign: "center" }}>Oh no!</Heading>
+      <Box style={{ textAlign: "center" }}>
+        <Heading>Oh no! Error 500</Heading>
+        <Text>There seems to have been an error on our part. </Text>
+        <Link href="/">Go home</Link>
+      </Box>
     </Box>
   );
 }
