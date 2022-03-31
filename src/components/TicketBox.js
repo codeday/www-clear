a80812fd7a9fefbd75116a7cd99dac17e5fedd09
Lola <lola@egherman.com>
2022-03-30 20:00:00 -0500

    show currency on event homepage

--- src/components/TicketBox.js
+++ src/components/TicketBox.js
@@ -9,14 +9,14 @@ export default function TicketBox({ event, children, ...props }) {
     return (
         <InfoBox headingSize="xl" heading="Pricing" {...props}>
             <Text mb={0}>
-                <PaymentDiscount />Early Bird Ticket Price: {event.earlyBirdPrice}
+                <PaymentDiscount />Early Bird Ticket Price: {event.region?.currencySymbol}{event.earlyBirdPrice}
             </Text>
             <Text mb={0}>
                 <Calendar />Early Bird Deadline: {moment(event.earlyBirdCutoff).utc().format('LL')}
             </Text>
             {event.canEarlyBirdRegister ? <GoodAlert>Early Bird Active</GoodAlert> : <Alert>Early Bird Inactive</Alert>}
             <Text mb={0} mt={2}>
-                <PaymentCash />Regular Ticket Price: {event.ticketPrice}
+                <PaymentCash />Regular Ticket Price: {event.region?.currencySymbol}{event.ticketPrice}
             </Text>
             <Text mb={0}>
                 <Calendar />Registration Deadline: {moment(event.registrationCutoff).utc().format('LL')}
