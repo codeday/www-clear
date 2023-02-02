e619214af030f6d656675e4765cc9f73d5bb1f0e
Lola <lola@egherman.com>
2023-02-01 18:37:58 -0600

    drop shadow instead of border

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -4,34 +4,34 @@ import {useColorModeValue} from "@codeday/topo/Theme";
 
 export default function InfoBox({children, heading, subHeading, headingSize, buttons, nested, ...props}) {
     return (
-        <Box
-            d="block"
-            borderWidth={3}
-            rounded={nested ? 1 : 3}
-            borderColor={useColorModeValue(nested ? 'gray.100' : undefined, nested ? 'gray.1200' : undefined)}
-            m={nested ? 0 : 1}
-            {...props}
-        >
-            {heading && (
-              <Box
-                backgroundColor={useColorModeValue(nested ? 'gray.100' : 'gray.200', nested ? 'gray.1000' : 'gray.1200')}
-                fontSize={headingSize}
-                p={2}
-              >
+      <Box
+        d="block"
+        borderWidth={0}
+        rounded={nested ? 1 : 3}
+        boxShadow="base"
+        m={nested ? 0 : 2}
+        {...props}
+      >
+          {heading && (
+            <Box
+              backgroundColor={useColorModeValue(nested ? 'gray.100' : 'gray.200', nested ? 'gray.1000' : 'gray.1200')}
+              fontSize={headingSize}
+              p={2}
+            >
                 <Flex fontWeight="bold" justifyContent="space-between">
                     <Box>{heading}</Box>
                     <Box>{buttons}</Box>
                 </Flex>
                 {subHeading && <Text color="current.textLight" fontSize="sm">{subHeading}</Text>}
-              </Box>
-            )}
-            <Box
-                mt={1}
-                rounded={5}
-                p={1}
-            >
-                {children}
             </Box>
-        </Box>
+          )}
+          <Box
+            mt={1}
+            rounded={5}
+            p={1}
+          >
+              {children}
+          </Box>
+      </Box>
     );
 }
