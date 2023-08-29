1a0afa171234ea136a22bb1080ac9b3c6db362f5
Lola <lola@egherman.com>
2023-08-29 16:27:36 -0500

    fix infobox typing

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -1,51 +1,49 @@
-import React, { ComponentType } from 'react';
+import React from 'react';
 import { Box, Flex, Text, BoxProps } from '@codeday/topo/Atom';
 import { useColorModeValue } from '@codeday/topo/Theme';
-import { forwardRef } from '@chakra-ui/system';
+import { forwardRef } from '@chakra-ui/react';
 
-type _InfoBoxProps = Partial<{
+type InfoBoxProps = Partial<{
   heading: React.ReactNode;
   subHeading: string;
   headingSize: BoxProps['fontSize'];
   buttons: React.ReactNode;
   nested: Boolean;
-}>;
+}> &
+  BoxProps;
 
 // This needs to be exported like this to make typing function correctly with the chakra-ui `as` prop
-// It still doesn't function exactly correctly but it's good enough for now
-export const InfoBox = forwardRef<_InfoBoxProps, 'div'>(
-  ({ children, heading, subHeading, headingSize, buttons, nested, ...props }, ref) => (
-    <Box
-      ref={ref}
-      display="block"
-      borderWidth={0}
-      rounded={nested ? 1 : 3}
-      boxShadow="base"
-      m={nested ? 0 : 2}
-      {...props}
-    >
-      {heading && (
-        <Box
-          backgroundColor={useColorModeValue(nested ? 'gray.100' : 'gray.200', nested ? 'gray.1000' : 'gray.1200')}
-          fontSize={headingSize}
-          p={2}
-        >
-          <Flex fontWeight="bold" justifyContent="space-between">
-            <Box>{heading}</Box>
-            <Box>{buttons}</Box>
-          </Flex>
-          {subHeading && (
-            <Text color="current.textLight" fontSize="sm">
-              {subHeading}
-            </Text>
-          )}
+// Other components that extend `InfoBox` also need to be exported like this for their `as` props to be properly typed
+export const InfoBox = forwardRef<InfoBoxProps, 'div'>(
+  ({ children, heading, subHeading, headingSize, buttons, nested, ...props }, ref) => {
+    const backgroundColor = useColorModeValue(nested ? 'gray.100' : 'gray.200', nested ? 'gray.1000' : 'gray.1200');
+    return (
+      <Box
+        ref={ref}
+        display="block"
+        borderWidth={0}
+        rounded={nested ? 1 : 3}
+        boxShadow="base"
+        m={nested ? 0 : 2}
+        {...props}
+      >
+        {heading && (
+          <Box backgroundColor={backgroundColor} fontSize={headingSize} p={2}>
+            <Flex fontWeight="bold" justifyContent="space-between">
+              <Box>{heading}</Box>
+              <Box>{buttons}</Box>
+            </Flex>
+            {subHeading && (
+              <Text color="current.textLight" fontSize="sm">
+                {subHeading}
+              </Text>
+            )}
+          </Box>
+        )}
+        <Box mt={1} rounded={5} p={1}>
+          {children}
         </Box>
-      )}
-      <Box mt={1} rounded={5} p={1}>
-        {children}
       </Box>
-    </Box>
-  ),
+    );
+  },
 );
-
-export type InfoBoxProps = Parameters<typeof InfoBox>[0];
